3b8ae1b050252f56f1f4888718c5275f452b81e6
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2022-01-02 15:34:55 -0500

    Add files via upload

---
+++ EBSExedenverialma/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EBSExedenverialma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog op;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                op = new OpenFileDialog();
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    ProcessStartInfo EBSİnfo = new ProcessStartInfo(op.FileName, textBox1.Text);
+                    Process.Start(EBSİnfo);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Çalıştırılacak Program belirtiniz..","EBS Time");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "EBS Time");
+            }
+        }
+    }
+}
